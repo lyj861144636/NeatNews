248f5310689d4c33f9a2dc52aeadfae7c9b9fa66
houyunpeng <1600892327@qq.com>
2016-01-21 10:46:58 +0800

    初始化 页面

---
+++ Xinwen/Xinwen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xinwen
+//
+//  Created by hyp on 16/1/21.
+//  Copyright © 2016年 hyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xinwen/Xinwen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xinwen
+//
+//  Created by hyp on 16/1/21.
+//  Copyright © 2016年 hyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
